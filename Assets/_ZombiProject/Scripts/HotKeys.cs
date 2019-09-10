7c132466fa1157e980ac73d1c72bd3021147b933
Agafonov Timur <3iks@mail.ru>
2019-09-10 21:01:57 +0300

    few ui windows, some improvements

--- Assets/_ZombiProject/Scripts/HotKeys.cs
+++ Assets/_ZombiProject/Scripts/HotKeys.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class HotKeys : MonoBehaviour {
 
+    [SerializeField]
+    GameObject shop;
+    bool isShopActive = false;
+    bool isOptActive = false;
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject opt;
+    
 	// Use this for initialization
 	void Start () {
         
@@ -12,12 +21,46 @@ public class HotKeys : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.O))
-        {
+        if (Input.GetKeyDown(KeyCode.Escape))
             Application.Quit();
-        }
-        if (Input.GetKeyDown(KeyCode.I))
+
+        if (Input.GetKeyDown(KeyCode.F5))
             SceneManager.LoadScene(1);
+
+        if (Input.GetKeyDown(KeyCode.B)) //  shop
+        {
+            opt.SetActive(false);
+            isOptActive = false;
+            if (isShopActive == false)
+            {
+                shop.SetActive(true);
+                player.SetActive(false);
+            }
+            if (isShopActive == true)
+            {
+                shop.SetActive(false);
+                player.SetActive(true);
+            }
+        isShopActive = !isShopActive;
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))  // options
+        {
+            shop.SetActive(false);
+            isShopActive = false;
+            if (isOptActive == false)
+            {
+                opt.SetActive(true);
+                player.SetActive(false);
+            }
+            if (isOptActive == true)
+            {
+                opt.SetActive(false);
+                player.SetActive(true);
+            }
+        isOptActive = !isOptActive;
+        }
+
         //if (Input.GetKeyDown(KeyCode.P) && Time.timeScale != 0)
         //{
         //    Time.timeScale = 0;
@@ -25,5 +68,5 @@ public class HotKeys : MonoBehaviour {
         //}
         //else Time.timeScale = 1;
 
-	}
+    }
 }
