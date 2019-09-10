2277a2ecf903cdb903614c0b1590b20b751f7d4e
Agafonov Timur <3iks@mail.ru>
2019-09-10 15:04:08 +0300

    partly added bot lvl

---
+++ Assets/_ZombiProject/Scripts/LiftActivation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftActivation : MonoBehaviour {
+
+    [SerializeField]
+    GameObject lift;
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        lift.GetComponent<BoxCollider>().enabled = true;
+    }
+}
